b56e68fd0e151d2479d6a4ee8761f487169b7870
chrisms150 <chrisms150@users.noreply.github.com>
2021-05-29 14:15:50 -0400

    Update outPostController.lua

--- outPostController.lua
+++ outPostController.lua
@@ -1,3 +1,13 @@
 wget = loadfile("/bin/wget.lua")
-print("loaded second time")
-wget("-f","https://raw.githubusercontent.com/chrisms150/AutomatedIR/main/outPostController.lua","test.lua")
+
+print("loaded from github")
+
+--wget("-f","https://raw.githubusercontent.com/chrisms150/AutomatedIR/main/outPostController.lua","test.lua")
+wget("-f","https://pastebin.com/raw/YFg6QSr8","test.lua")
+i = 0
+
+while true do
+	print(i)
+	i = i+1
+	os.sleep(1)
+end
